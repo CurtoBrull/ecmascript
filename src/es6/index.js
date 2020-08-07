8628ff162800f4024a406c8d74cd6272b2aff761
CurtoBrull <curto.brull.javier@gmail.com>
2020-08-07 22:14:19 +0200

    ES6 completo

--- src/es6/index.js
+++ src/es6/index.js
@@ -66,3 +66,95 @@ let team2 = ['Ana', 'Silvia', 'Sara'];
 let education = ['David', ...team1, ...team2];
 
 console.log(education);
+
+// Objetos
+
+let name = 'Oscar';
+let age = 32;
+
+obj = {name: name, age: age};
+
+// ES6
+
+obj2 = {name, age};
+
+console.log(obj2);
+
+// Arrow funtions. Sintaxis más reducida y .this no vinculable
+
+const names = [
+  {name: 'Javier', age: 41},
+  {name: 'Loly', age: 47}
+]
+
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+})
+// Arrow function
+let listOfNames2 = names.map(item => console.log(item.name))
+
+const listOfNames3 = (name, age, country) => {
+  ...
+}
+
+const listOfNames4 = name => {
+  ...
+}
+
+// Arrow function que multiplica un numero por si mismo
+const square = num => num * num;
+
+// Promesas (algo va a pasar). Asincronismo
+
+const helloPromise = () => {
+  return new Promise ((resolve, reject) => {
+    if (false) {
+      resolve('Hey!');
+    } else {
+      reject('Ups!!');
+    }
+  });
+}
+helloPromise()
+  .then(response => console.log(response))
+  .then(() => console.log('hola'))
+  .catch(error => console.log(error));
+
+// Clases
+
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum (valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+
+// Import - Export
+
+import { hello } from './module';
+
+hello();
+
+// Generators
+
+function* helloWorld() {
+  if (true) {
+    yield 'Hello, ';
+  } 
+  if (true) {
+    yield 'World';
+  }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value); // hello
+console.log(generatorHello.next().value); // world
+console.log(generatorHello.next().value); // undefined
